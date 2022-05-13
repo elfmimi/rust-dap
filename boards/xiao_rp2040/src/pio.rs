f25bc425c740d1041e22f4492690218bd5b7e210
Ein Terakawa <applause@elfmimi.jp>
2022-05-13 23:10:55 +0900

    pio-almost-done

--- boards/xiao_rp2040/src/pio.rs
+++ boards/xiao_rp2040/src/pio.rs
@@ -36,8 +36,9 @@ pub trait DelayFunc {
 pub struct SwdIoSet<C, D, DelayFn>
     where DelayFn: DelayFunc,
 {
-    clk_pin_id: u8,
-    dat_pin_id: u8,
+    // We only need these infomation at initialization in fn new() .
+    // clk_pin_id: u8,
+    // dat_pin_id: u8,
     cycle_delay: DelayFn,
     running_sm: Option<hal::pio::StateMachine<hal::pio::PIO0SM0, hal::pio::Running>>,
     rx_fifo: hal::pio::Rx<hal::pio::PIO0SM0>,
@@ -78,53 +79,91 @@ impl <C, D, DelayFn> SwdIoSet<pio0::Pin<C>, pio0::Pin<D>, DelayFn>
         let mut a = pio::Assembler::<{pio::RP2040_MAX_PROGRAM_SIZE}>::new();
         a.side_set = pio::SideSet::new(true, 1, false);
         let mut write_loop = a.label();
+        let mut write_loop_enter = a.label();
         let mut read_start = a.label();
         let mut read_loop = a.label();
+        let mut read_loop_enter = a.label();
         let mut wrap_target = a.label();
         // let mut wrap_source = a.label();
         const HI: u8 = 1;
         const LO: u8 = 0;
+        const Q: u8 = 3 - 1; // delay
         // Set SWCLK pin as output.
         // a.set(pio::SetDestination::PINDIRS, 1);
         a.bind(&mut wrap_target);
+        // Get number of bits and direction bit from FIFO to OSR.
         a.pull(false, true);
-        // Copy 1-bit from OSR to ISR.
-        a.r#in(pio::InSource::OSR, 1);
-        // Clear other 31-bit of ISR.
-        a.r#in(pio::InSource::NULL, 31);
-        // Set IO direction of SWDIO pin.
-        a.out(pio::OutDestination::PINDIRS, 1);
-        // Copy OSR X register, which specifies number of bits.
-        a.mov(pio::MovDestination::X, pio::MovOperation::None, pio::MovSource::OSR);
-        // Copy ISR to Y register.
-        a.mov(pio::MovDestination::Y, pio::MovOperation::None, pio::MovSource::ISR);
+        // Move number of bits to X register.
+        a.out(pio::OutDestination::X, 31);
+        // Copy direction bit to Y register.
+        a.mov(pio::MovDestination::Y, pio::MovOperation::None, pio::MovSource::OSR);
         // Y == 0 means Read , Y != 0 means Write
         a.jmp(pio::JmpCondition::YIsZero, &mut read_start);
 
         // Write-Bits
         // Get data from FIFO to OSR.
         a.pull(false, true);
+        // We want to prepare output value before setting pin direction.
+        a.out(pio::OutDestination::PINS, 1);
+        // Use ISP as a temporary store.
+        a.mov(pio::MovDestination::ISR, pio::MovOperation::None, pio::MovSource::OSR);
+        // Y register has value 1. Copy that 1 to OSR.
+        a.mov(pio::MovDestination::OSR, pio::MovOperation::None, pio::MovSource::Y);
+        // Set IO direction of SWDIO pin to output.
+        a.out(pio::OutDestination::PINDIRS, 1);
+        // Restore rest of data from ISR to OSR.
+        a.mov(pio::MovDestination::OSR, pio::MovOperation::None, pio::MovSource::ISR);
+        // Jump if X register is not 0. with post decrement.
+        a.jmp(pio::JmpCondition::XDecNonZero, &mut write_loop_enter);
+
+        // We have updated state of SWDIO pin while keeping SWCLK static.
+        // Start over.
+        a.jmp(pio::JmpCondition::Always, &mut wrap_target);
+
         a.bind(&mut write_loop);
-        // Output 1-bit. and set SWCLK to Low.
-        a.out_with_side_set(pio::OutDestination::PINS, 1, LO);
-        // Keep looping unless X register is 0. and set SWCLK to High.
-        a.jmp_with_side_set(pio::JmpCondition::XDecNonZero, &mut write_loop, HI);
-        // Set SWCLK to Low.
-        a.set(pio::SetDestination::PINS, LO);
+        // Output 1-bit. and set SWCLK to High.
+        a.set(pio::SetDestination::PINS, HI);
+        a.out_with_delay(pio::OutDestination::PINS, 1, match Q { 0 => 0, _ => Q - 1 });
+        a.bind(&mut write_loop_enter);
+        // Keep looping unless X register is 0. and set SWCLK to Low.
+        a.jmp_with_delay_and_side_set(pio::JmpCondition::XDecNonZero, &mut write_loop, Q, LO);
+        // Set SWCLK to High.
+        a.set(pio::SetDestination::PINS, HI);
+        // a.set_with_delay(pio::SetDestination::PINS, HI, Q);
+        // Start over.
         a.jmp(pio::JmpCondition::Always, &mut wrap_target);
 
         // Read-Bits
         a.bind(&mut read_start);
+        // Set IO direction of SWDIO pin to input.
+        a.out_with_delay(pio::OutDestination::PINDIRS, 1, match Q { 0 => 0, _ => Q - 1 });
+        // Jump if X register is not 0. with post decrement.
+        a.jmp(pio::JmpCondition::XDecNonZero, &mut read_loop_enter);
+        // Wait before reading SWDIO pin.
+        a.nop_with_delay(Q);
+        // Read state of SWDIO pin without clocking SWCLK.
+        a.r#in(pio::InSource::PINS, 1);
+        // Shift in 31 bits of 0 to MSB of ISR.
+        a.r#in(pio::InSource::NULL, 31);
+        // Put result data from ISR to FIFO.
+        a.push(false, true);
+        // Start over.
+        a.jmp(pio::JmpCondition::Always, &mut wrap_target);
+
         // Set SWCLK to Low.
-        a.set(pio::SetDestination::PINS, LO);
+        // a.set(pio::SetDestination::PINS, LO);
         a.bind(&mut read_loop);
         // Shift-in 1-bit to ISR. and set SWCLK to High.
-        a.r#in_with_side_set(pio::InSource::PINS, 1, HI);
+        a.r#in_with_delay_and_side_set(pio::InSource::PINS, 1, Q, HI);
+        a.bind(&mut read_loop_enter);
         // Keep looping unless X register is 0. and set SWCLK to Low.
-        a.jmp_with_side_set(pio::JmpCondition::XDecNonZero, &mut read_loop, LO);
+        a.jmp_with_delay_and_side_set(pio::JmpCondition::XDecNonZero, &mut read_loop, Q, LO);
+        a.r#in_with_side_set(pio::InSource::PINS, 1, HI);
+        // a.r#in_with_delay_and_side_set(pio::InSource::PINS, 1, Q, HI);
         // Put result data from ISR to FIFO.
         a.push(false, true);
         // a.bind(&mut wrap_source);
+        // Start over.
         a.jmp(pio::JmpCondition::Always, &mut wrap_target);
 
         // The labels wrap_target and wrap_source, as set above,
@@ -137,7 +176,9 @@ impl <C, D, DelayFn> SwdIoSet<pio0::Pin<C>, pio0::Pin<D>, DelayFn>
         // Initialize and start PIO
         let (mut pio, sm0, _, _, _) = pio0.split(resets);
         let installed = pio.install(&program).unwrap();
-        let div = 25f32; // 125MHz / 25 = 5Mhz
+        // let div = 25f32; // 125MHz / 25 = 5Mhz
+        // let div = 5f32; // 125MHz / 5 = 25Mhz
+        let div = 1f32; // 125MHz / 1 = 125Mhz
         let (mut sm, rx, tx) = hal::pio::PIOBuilder::from_program(installed)
             .set_pins(clk_pin_id, 1)
             .side_set_pin_base(clk_pin_id)
@@ -151,8 +192,8 @@ impl <C, D, DelayFn> SwdIoSet<pio0::Pin<C>, pio0::Pin<D>, DelayFn>
         let running_sm = sm.start();
 
         Self {
-            clk_pin_id,
-            dat_pin_id,
+            // clk_pin_id,
+            // dat_pin_id,
             cycle_delay: cycle_delay,
             running_sm: Some(running_sm),
             rx_fifo: rx,
@@ -166,25 +207,23 @@ impl <C, D, DelayFn> SwdIoSet<pio0::Pin<C>, pio0::Pin<D>, DelayFn>
 impl<C, D, DelayFn> BasicSwdIo for SwdIoSet<C, D, DelayFn>
     where DelayFn: DelayFunc
 {
+    // if bits > 32 , it will behave as if value is extended with zeros.
     fn write_bits(&mut self, bits: u32, value: u32) {
-        self.tx_fifo.write((bits - 1) << 1 | 1);
-        self.tx_fifo.write(value);
-        // Wait for start
-        while !self.tx_fifo.is_empty() { }
-        // Wait for completion
-        self.running_sm.as_ref().map(|sm| while sm.instruction_address() != 0 { });
+        while !self.tx_fifo.write(bits | 1 << 31) { }
+        while !self.tx_fifo.write(value) { }
     }
+    // if bits > 32 , result contains only the last 32 bits.
     fn read_bits(&mut self, bits: u32) -> u32 {
-        self.tx_fifo.write((bits - 1) << 1 | 0);
+        while !self.tx_fifo.write(bits | 0) { }
         while self.rx_fifo.is_empty() { }
         let value = self.rx_fifo.read().unwrap();
-        value >> (32 - bits)
+        value >> ((32 - bits) & 31)
     }
-    fn set_output_enable(&mut self, oe: bool) {
-        let mut stopped_sm = self.running_sm.take().unwrap().stop();
-        use hal::pio::PinDir::{Input, Output};
-        stopped_sm.set_pindirs([(self.dat_pin_id, match oe { true => Output, false => Input })]);
-        self.running_sm = Some(stopped_sm.start());
+    fn to_swdio_in(&mut self) {
+        self.read_bits(0);
+    }
+    fn to_swdio_out(&mut self, output: bool) {
+        self.write_bits(0, match output { true => 1, false => 0 });
     }
 }
 
@@ -266,7 +305,9 @@ pub trait BitBangSwdIo {
 pub trait BasicSwdIo {
     fn write_bits(&mut self, bits: u32, value: u32);
     fn read_bits(&mut self, bits: u32) -> u32;
-    fn set_output_enable(&mut self, oe: bool);
+    fn to_swdio_in(&mut self);
+    fn to_swdio_out(&mut self, output: bool);
+    // fn set_swdio_output(&mut self, output: bool);
 }
 
 pub trait PrimitiveSwdIo {
@@ -419,36 +460,20 @@ impl<C, D, DelayFn> PrimitiveSwdIo for SwdIoSet<C, D, DelayFn>
         // self.to_swdio_in();
     }
     fn enable_output(&mut self) {
-        // self.to_swdio_out(false);
-        // Program will be stopped if running.
-        self.set_output_enable(true);
+        self.to_swdio_out(false);
     }
     fn disable_output(&mut self) {
-        // self.to_swdio_in();
-        // Program will be stopped if running.
-        self.set_output_enable(false);
+        self.to_swdio_in();
     }
 
     fn cycle_clock(&mut self, _config: &SwdIoConfig) {
         self.read_bits(1);
     }
     fn turn_around(&mut self, config: &SwdIoConfig) {
-/*
-        for _ in 0..config.turn_around_cycles {
-            self.cycle_clock(config);
-        }
-*/
-        // implicit assumption: turn_around_cycles > 0, and <= 32
         self.read_bits(config.turn_around_cycles);
     }
     fn idle_cycle(&mut self, config: &SwdIoConfig) {
-/*
-        for _ in 0..config.idle_cycles {
-            self.write_bit(config, false);
-        }
-*/
         if config.idle_cycles != 0 {
-            // implicit assumption: config.idle_cycles <= 32
             self.write_bits(config.idle_cycles, 0);
         }
     }
@@ -700,24 +725,29 @@ impl<C, D, DelayFn> SwdIo for SwdIoSet<C, D, DelayFn>
             index += 1;
         }
     }
-
     fn swd_write_sequence(&mut self, config: &SwdIoConfig, count: usize, data: &[u8]) {
-        let mut count = count;
+        // count > 0 is assured
+        let mut count: u32 = count as u32;
         let mut index = 0;
-        while count > 0 {
-            let mut value = data[index];
+        let mut bits = 0;
+        let mut value = 0;
+        loop {
+            value |= (data[index] as u32) << bits;
             index += 1;
-            let mut bits = 8;
-            while bits > 0 && count > 0 {
-                bits -= 1;
-                count -= 1;
-
-                self.write_bit(config, value & 1 != 0);
-                value >>= 1;
+            bits += 8;
+            if count <= bits {
+                bits = count;
+                self.write_bits(bits, value);
+                break;
+            }
+            if bits == 32 {
+                self.write_bits(bits, value);
+                count -= bits;
+                bits = 0;
+                value = 0;
             }
         }
     }
-
     fn swd_transfer(
         &mut self,
         config: &SwdIoConfig,
@@ -725,60 +755,37 @@ impl<C, D, DelayFn> SwdIo for SwdIoSet<C, D, DelayFn>
         data: u32,
     ) -> core::result::Result<u32, DapError> {
         if self.transfer_count % 1000 == 0 {
-            println!("swd_transfer! {0}", self.transfer_count);
+            // println!("swd_transfer! {0}", self.transfer_count);
         }
         self.transfer_count += 1;
         // write request
         // self.enable_output();
         // SwdIo::enable_output(self);
-        PrimitiveSwdIo::enable_output(self);
+        // PrimitiveSwdIo::enable_output(self);
         {
-            let mut parity = false;
-            self.write_bit(config, true); // Start
-            let bit = request.contains(SwdRequest::APnDP);
-            self.write_bit(config, bit);
-            parity ^= bit;
-            let bit = request.contains(SwdRequest::RnW);
-            self.write_bit(config, bit);
-            parity ^= bit;
-            let bit = request.contains(SwdRequest::A2);
-            self.write_bit(config, bit);
-            parity ^= bit;
-            let bit = request.contains(SwdRequest::A3);
-            self.write_bit(config, bit);
-            parity ^= bit;
-            self.write_bit(config, parity); // Parity
-            self.write_bit(config, false); // Stop
-            self.write_bit(config, true); // Park
+            let mut bits = request.bits() & 0b1111;
+            bits |= (bits.count_ones() as u8 & 1) << 4;
+            bits = (bits << 1) | 0x81;
+            self.write_bits(8, bits as u32);
         }
 
-        // turnaround + read ack.
+        // turnaround + read ack. ( + turnaround, for write request )
         // self.disable_output();
-        // SwdIo::disable_output(self);
-        PrimitiveSwdIo::disable_output(self);
-        self.turn_around(config);
-        let ack = {
-            let mut ack = 0u8;
-            ack |= if self.read_bit(config) { 0b001 } else { 0b000 };
-            ack |= if self.read_bit(config) { 0b010 } else { 0b000 };
-            ack |= if self.read_bit(config) { 0b100 } else { 0b000 };
-            ack
-        };
+        let ack =
+            if request.contains(SwdRequest::RnW) {
+                self.read_bits(3 + config.turn_around_cycles) as u8 >> config.turn_around_cycles & 0b111
+            } else {
+                self.read_bits(3 + config.turn_around_cycles * 2) as u8 >> config.turn_around_cycles & 0b111
+            };
         if ack == DAP_TRANSFER_OK {
             let ack = if request.contains(SwdRequest::RnW) {
                 // READ request
-                let mut value = 0u32;
-                let mut parity = false;
-                for _ in 0..32 {
-                    let bit = self.read_bit(config);
-                    parity ^= bit;
-                    value = (value >> 1) | if bit { 0x80000000 } else { 0x00000000 };
-                }
-                let parity_expected = self.read_bit(config);
-                self.turn_around(config);
+                let value = self.read_bits(32);
+                let parity = value.count_ones() & 1;
+                // let parity_expected = self.read_bit(config);
+                // self.turn_around(config);
+                let parity_expected = self.read_bits(1 + config.turn_around_cycles) & 1;
                 // self.enable_output();
-                // SwdIo::enable_output(self);
-                PrimitiveSwdIo::enable_output(self);
                 if parity == parity_expected {
                     Ok(value)
                 } else {
@@ -786,24 +793,16 @@ impl<C, D, DelayFn> SwdIo for SwdIoSet<C, D, DelayFn>
                 }
             } else {
                 // WRITE request
-                self.turn_around(config);
+                // self.turn_around(config);
                 // self.enable_output();
-                // SwdIo::enable_output(self);
-                PrimitiveSwdIo::enable_output(self);
-                let mut value = data;
-                let mut parity = false;
-                for _ in 0..32 {
-                    let bit = value & 1 != 0;
-                    self.write_bit(config, bit);
-                    parity ^= bit;
-                    value >>= 1;
-                }
-                self.write_bit(config, parity);
+                self.write_bits(32, data);
+                self.write_bits(1, data.count_ones());
                 Ok(0)
             };
             // TODO: capture timestamp
             self.idle_cycle(config);
-            self.set_swdio(true);
+            // self.set_swdio(true);
+            self.to_swdio_out(true);
             return ack;
         }
 
@@ -813,32 +812,42 @@ impl<C, D, DelayFn> SwdIo for SwdIoSet<C, D, DelayFn>
             // SwdIo::disable_output(self);
             PrimitiveSwdIo::disable_output(self);
             if config.always_generate_data_phase && request.contains(SwdRequest::RnW) {
+/*
                 for _ in 0..33 {
                     self.cycle_clock(config);
                 }
+*/
+                self.read_bits(33);
             }
             self.turn_around(config);
             // self.enable_output();
-            // SwdIo::enable_output(self);
-            PrimitiveSwdIo::enable_output(self);
             if config.always_generate_data_phase && !request.contains(SwdRequest::RnW) {
+/*
                 for _ in 0..33 {
                     self.write_bit(config, false);
                 }
+*/
+                self.write_bits(33, 0);
             }
-            self.set_swdio(true);
+            // self.set_swdio(true);
+            self.to_swdio_out(true);
             return Err(DapError::SwdError(ack));
         }
 
         // Protocol error
-        self.turn_around(config);
+        // self.turn_around(config);
+/*
         for _ in 0..33 {
             self.cycle_clock(config);
         }
+*/
+        self.read_bits(33);
+        if request.contains(SwdRequest::RnW) {
+            self.turn_around(config);
+        }
         // self.enable_output();
-        // SwdIo::enable_output(self);
-        PrimitiveSwdIo::enable_output(self);
-        self.set_swdio(true);
+        // self.set_swdio(true);
+        self.to_swdio_out(true);
         return Err(DapError::SwdError(ack));
     }
 
