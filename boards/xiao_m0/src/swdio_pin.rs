438d25cb29c60f6b6f14e107ea048964d335da92
Ein Terakawa <applause@elfmimi.jp>
2024-10-28 00:08:45 +0900

    embedded-hal 1.0 WIP 4

--- boards/xiao_m0/src/swdio_pin.rs
+++ boards/xiao_m0/src/swdio_pin.rs
@@ -1,5 +1,6 @@
-use embedded_hal::digital::v2::{InputPin, IoPin, OutputPin, PinState};
-use xiao_m0::hal::gpio::v2::pin::{Disabled, Floating, Input, Output, Pin, PinId, PushPull};
+use embedded_hal::digital::{InputPin, OutputPin, PinState, ErrorType};
+use xiao_m0::hal::gpio::pin::{Floating, Input, Output, Pin, PinId, PushPull};
+use rust_dap::bitbang::IoPin;
 
 pub struct XiaoSwdInputPin<I>
 where
@@ -17,15 +18,21 @@ where
     }
 }
 
-impl<I> InputPin for XiaoSwdInputPin<I>
+impl<I> ErrorType for XiaoSwdInputPin<I>
 where
     I: PinId,
 {
     type Error = core::convert::Infallible;
-    fn is_high(&self) -> Result<bool, Self::Error> {
+}
+
+impl<I> InputPin for XiaoSwdInputPin<I>
+where
+    I: PinId,
+{
+    fn is_high(&mut self) -> Result<bool, Self::Error> {
         self.pin.is_high()
     }
-    fn is_low(&self) -> Result<bool, Self::Error> {
+    fn is_low(&mut self) -> Result<bool, Self::Error> {
         self.pin.is_low()
     }
 }
@@ -34,7 +41,6 @@ impl<I> IoPin<XiaoSwdInputPin<I>, XiaoSwdOutputPin<I>> for XiaoSwdInputPin<I>
 where
     I: PinId,
 {
-    type Error = core::convert::Infallible;
     fn into_input_pin(self) -> Result<XiaoSwdInputPin<I>, Self::Error> {
         Ok(self)
     }
@@ -61,18 +67,24 @@ where
     }
 }
 
-impl<I> OutputPin for XiaoSwdOutputPin<I>
+impl<I> ErrorType for XiaoSwdOutputPin<I>
 where
     I: PinId,
 {
     type Error = core::convert::Infallible;
+}
+
+impl<I> OutputPin for XiaoSwdOutputPin<I>
+where
+    I: PinId,
+{
     fn set_high(&mut self) -> Result<(), Self::Error> {
         self.pin.set_high()
     }
     fn set_low(&mut self) -> Result<(), Self::Error> {
         self.pin.set_low()
     }
-    fn set_state(&mut self, state: embedded_hal::digital::v2::PinState) -> Result<(), Self::Error> {
+    fn set_state(&mut self, state: PinState) -> Result<(), Self::Error> {
         self.pin.set_state(state)
     }
 }
@@ -81,7 +93,6 @@ impl<I> IoPin<XiaoSwdInputPin<I>, XiaoSwdOutputPin<I>> for XiaoSwdOutputPin<I>
 where
     I: PinId,
 {
-    type Error = core::convert::Infallible;
     fn into_input_pin(self) -> Result<XiaoSwdInputPin<I>, Self::Error> {
         let new_pin = self.pin.into_floating_input();
         Ok(XiaoSwdInputPin::new(new_pin))
@@ -92,6 +103,7 @@ where
     }
 }
 
+/*
 pub struct XiaoSwdPin<I>
 where
     I: PinId,
@@ -99,11 +111,17 @@ where
     pin: Pin<I, Disabled<Floating>>,
 }
 
+impl<I> ErrorType for XiaoSwdPin<I>
+where
+    I: PinId
+{
+    type Error = core::convert::Infallible;
+}
+
 impl<I> IoPin<XiaoSwdInputPin<I>, XiaoSwdOutputPin<I>> for XiaoSwdPin<I>
 where
     I: PinId,
 {
-    type Error = core::convert::Infallible;
     fn into_input_pin(self) -> Result<XiaoSwdInputPin<I>, Self::Error> {
         let input_pin = self.pin.into_floating_input();
         Ok(XiaoSwdInputPin::new(input_pin))
@@ -115,3 +133,4 @@ where
         Ok(new_pin)
     }
 }
+*/
